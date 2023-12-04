396e025ee63564f88321e0158ed14a35ab7e019d
PikkaStar <yokotaka1226@gmail.com>
2023-12-04 04:46:15 +0000

    registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
+
+  devise_for :admins,skip: [:registrations,:passwords],controllers: {
+    sessions: "admin/sessions"
+  }
+  devise_for :users,skip: [:passwords],controllers: {
+    registrations: "user/registrations",
+    sessions: "user/sessions"
+  }
+
+  namespace :admin do
+    root to: "homes#top"
+  end
+
+  scope module: :admin do
+    resources :posts,only: [:index,:show,:edit,:destroy]
+    resources :users,only: [:index,:show,:edit,:update]
+  end
+
+  scope module: :user do
+    root to: 'homes#top'
+    get 'about' => 'homes#about'
+    resources :posts
+    resources :users,only: [:index,:show,:edit,:update]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
