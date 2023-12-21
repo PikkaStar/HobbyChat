51d0f77c9479df8d838a6873e7d2f77310b6028a
PikkaStar <yokotaka1226@gmail.com>
2023-12-21 00:06:28 +0000

    js groupseed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,28 @@ import "jquery";
 import "popper.js";
 import "bootstrap";
 import "../stylesheets/application";
+
+$(function() {
+  $('.images').slick({
+      dots: true,
+      autoplay: true, // 自動再生を有効にする
+      autoplaySpeed: 2000, // 自動再生の速さ（ミリ秒単位）
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+      document.getElementById("confirmationButtons").style.display = "block";
+       // フェードインのアニメーション
+      let opacity = 0;
+      const fadeInInterval = setInterval(function () {
+        if (opacity < 1) {
+          opacity += 0.1;
+          confirmationButtons.style.opacity = opacity;
+        } else {
+          clearInterval(fadeInInterval);
+        }
+      }, 100);
+    }, 4000); // 3000ミリ秒 = 3秒
+  });
+
