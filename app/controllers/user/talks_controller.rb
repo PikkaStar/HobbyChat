de390752b423a3b57c8e183894810e4dfb661505
PikkaStar <yokotaka1226@gmail.com>
2024-01-18 02:51:53 +0000

    comment

--- app/controllers/user/talks_controller.rb
+++ app/controllers/user/talks_controller.rb
@@ -3,6 +3,7 @@ class User::TalksController < ApplicationController
 
   def create
     @area = Area.find(params[:area_id])
+    # talkエリアが存在するならtalkをcreateできる
     if Entry.where(user_id: current_user.id,area_id: params[:talk][:area_id]).present?
       @message = Talk.create(params.require(:talk).permit(:user_id, :message, :area_id).merge(user_id: current_user.id))
     else
